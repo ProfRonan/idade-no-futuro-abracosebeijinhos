208abdb8855c9b1ce2606ae665411e6dff100199
Gabriela Oliveira <130765819+abracosebeijinhos@users.noreply.github.com>
2023-04-18 15:11:11 -0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+ano_atual = int(input())
+idade_atual = int(input())
+outro_ano = int(input())
+nome = input()
+cálculo = ano_atual - outro_ano
+print(f"{nome}, no ano de {outro_ano} você terá {cálculo} anos")
+#Fulano, no ano de XXXX você terá YYYY anos
